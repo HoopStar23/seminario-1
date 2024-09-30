d8c8cd1ee56d18c84fcec069545f09a70a4dbf48
Hakeem <sb.hakeem99@gmail.com>
2024-09-30 18:13:31 +0200

    Comentarios en Menu Providers

--- lib/src/providers/menu_providers.dart
+++ lib/src/providers/menu_providers.dart
@@ -1,4 +1,4 @@
-import 'dart:convert';
+import 'dart:convert';//Para parsear JSON 
 
 import 'package:flutter/services.dart' show rootBundle;
 
@@ -9,10 +9,10 @@ class _MenuProvider{
     //cargarData();
   }
 
-  Future<List<dynamic>> cargarData() async{
-    final resp = await rootBundle.loadString('data/menu_opts.json');
+  Future<List<dynamic>> cargarData() async{//Future  representa un valor que se esta preparando para el futuro. Ya que no esta lista inmediatemente. Un async simepre devuelve un Future
+    final resp = await rootBundle.loadString('data/menu_opts.json');// Cargar JSON
 
-      Map dataMap = json.decode(resp);
+      Map dataMap = json.decode(resp);//El JSON es convertido en un Dart Map
       print(dataMap['rutas']);
       opciones = dataMap['rutas'];
 

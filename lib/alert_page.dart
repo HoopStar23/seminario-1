486808eb636d834082074d75f8fb8d31fc3b3c22
Hakeem <sb.hakeem99@gmail.com>
2024-10-01 15:33:34 +0200

    Alert Page Pop Up

--- lib/alert_page.dart
+++ lib/alert_page.dart
@@ -7,11 +7,45 @@ class AlertPage extends StatelessWidget{
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          child:Text('Mostrar Alerta'),
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.blue,
+            overlayColor: Colors.white,
+            shape: StadiumBorder()),
+          onPressed: () => _mostrarAlerta(context),
+          ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add_location),
         onPressed:() {
           Navigator.pop(context);
         }),
     );
+  }
+
+  void _mostrarAlerta(BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context){
+        return AlertDialog(
+          shape: 
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Este es el contenido de la caja de la alerta'),
+              FlutterLogo(size: 70)
+            ],
+          ),
+          actions: [
+            TextButton(onPressed: () => Navigator.of(context).pop(), child: Text('Cancelar')),
+            TextButton(onPressed: () => Navigator.of(context).pop(), child: Text('Ok'))
+          ],
+        );
+      });
   }
 }

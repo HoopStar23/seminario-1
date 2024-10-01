abe14484b15f863ba63da48f38c16b926172ac05
Hakeem <sb.hakeem99@gmail.com>
2024-10-01 16:08:04 +0200

    Animated Container

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -3,12 +3,14 @@ import 'package:seminario_1/alert_page.dart';
 import 'package:seminario_1/avatar_page.dart';
 import 'package:seminario_1/home_page.dart';
 import 'package:seminario_1/card_page.dart';
+import 'package:seminario_1/animated_container.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
   return <String, WidgetBuilder>{
     '/':(BuildContext context) => HomePage(),
     'alert':(BuildContext context) => AlertPage(),
     'avatar':(BuildContext context) => AvatarPage(),
-    'card':(BuildContext context) => CardPage()
+    'card':(BuildContext context) => CardPage(),
+    'animatedContainer':(BuildContext context) => AnimatedContainerPage(),
   };
 }

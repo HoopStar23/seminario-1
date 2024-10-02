067ea233e9a69593c29e230e362eacb8e57f9423
hakeem <sb.hakeem99@gmail.com>
2024-10-02 09:30:42 +0200

    Lista Infinita

--- lib/listview_page.dart
+++ lib/listview_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class ListaPage extends StatefulWidget{
@@ -6,30 +7,94 @@ class ListaPage extends StatefulWidget{
 }
 
 class _ListaPageState extends State<ListaPage>{
-  List<int> _listaNumeros = [1,2,3,4,5];
+  List<int> _listaNumeros = [];
+  int _lastItem = 0;
+  ScrollController _scrollController = new ScrollController();
+  bool _isLoading = false;
+
+  @override
+  void initState(){
+    super.initState();
+    _agregar10();
+
+    _scrollController.addListener((){
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+        fetchData();
+      }
+    });
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+    _scrollController.dispose();
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista'),
       ),
-      body: Center(
-        child: _crearLista(),
-      ),
+      body: Stack(children: [_crearLista(), _crearLoading()]),
     );
   }
 
 
   Widget _crearLista(){
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _listaNumeros.length,
       itemBuilder: (BuildContext context, int index){
         final imagen = _listaNumeros[index];
 
         return FadeInImage(
+          fit: BoxFit.cover,
+          height: 250,
           placeholder: AssetImage('assets/jar-loading.gif'),
           image: NetworkImage('https://picsum.photos/500/300?image=$imagen'));
       },
     );
+  }
+
+  Future fetchData() async{
+    _isLoading = true;
+    setState(() {});
+
+    final duration = new Duration(seconds: 2);
+    return new Timer(duration, responseHTTP);
+  }
+  void responseHTTP(){
+    _isLoading = false;
+
+    _scrollController.animateTo(_scrollController.position.pixels + 100,
+    curve: Curves.fastOutSlowIn, duration:  Duration(milliseconds:  250));
+    _agregar10();
+  }
+
+  void _agregar10(){
+    for (var i = 0; i < 10; i++) {
+      _lastItem++;
+      _listaNumeros.add(_lastItem);
+    }
+    setState(() {});
+  }
+
+  Widget _crearLoading(){
+    if (_isLoading) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [CircularProgressIndicator()],
+          ),
+          SizedBox(height: 15.0,)
+        ],
+      );
+    }else{
+      return Container();
+    }
   }
 }

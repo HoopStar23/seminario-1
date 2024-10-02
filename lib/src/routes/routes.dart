9beaeaf082cc4b3ac5ec2ab84f76ca3ea65f885b
hakeem <sb.hakeem99@gmail.com>
2024-10-02 08:36:04 +0200

    Slider Page

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:seminario_1/home_page.dart';
 import 'package:seminario_1/card_page.dart';
 import 'package:seminario_1/animated_container.dart';
 import 'package:seminario_1/input_page.dart';
+import 'package:seminario_1/slider_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
   return <String, WidgetBuilder>{
@@ -14,5 +15,6 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
     'card':(BuildContext context) => CardPage(),
     'animatedContainer':(BuildContext context) => AnimatedContainerPage(),
     'inputs':(BuildContext context) => InputPage(),
+    'slider': (BuildContext context) => SliderPage(),
   };
 }
